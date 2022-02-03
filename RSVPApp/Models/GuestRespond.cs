7471a24e60fc942ad655640e06cea4b9d57f81b4
Iulia Zirin <Iulia.Zirin@stefanini.com>
2022-02-03 17:11:21 +0200

    Invitation app

---
+++ RSVPApp/Models/GuestRespond.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RSVPApp.Models
+{
+    public class GuestRespond
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your phone")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Please specify whether you will attend")]
+        public bool? Attend { get; set; }
+
+    }
+}
