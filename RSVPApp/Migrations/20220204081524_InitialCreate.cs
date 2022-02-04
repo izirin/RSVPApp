e9e98d13c086e8ffa3c3a11f72cb467be03521c7
Iulia Zirin <Iulia.Zirin@stefanini.com>
2022-02-04 12:19:31 +0200

    DataBase

---
+++ RSVPApp/Migrations/20220204081524_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RSVPApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GuestRespond",
+                columns: table => new
+                {
+                    GuestRespondId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(30)", nullable: false),
+                    Attend = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GuestRespond", x => x.GuestRespondId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GuestRespond");
+        }
+    }
+}
